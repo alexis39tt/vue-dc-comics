0e7d4098075ed90b1c9fe688d9a9d394f480035b
alexis39tt <alessandro.fonzi@gmail.com>
2022-04-19 15:27:07 +0200

    finished

---
+++ src/components/CardComp.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card">
+    <div class="cardimg">
+      <img :src="card.thumb" :alt="card.series" />
+    </div>
+    <div class="cardtext">
+      <p>{{ card.series }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardComp",
+  props: {
+    card: Object,
+  },
+};
+</script>
+
+<style lang="scss">
+.card {
+    margin: 0 15px 0;
+    cursor: pointer;
+    transition: 0.2s;
+    &:hover{
+        transform: scale(1.1);
+        .cardtext{
+            color: #0a82f9;
+        }
+    }
+    .cardimg{
+        width: 100%;
+        height: 60%;
+        overflow: hidden;
+    }
+    .cardtext {
+        transition: 0.2s;
+    p {
+      text-transform: uppercase;
+      max-width: 192px;
+      margin-top: 15px;
+      max-height: 50px;
+    }
+  }
+}
+</style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,32 +1,80 @@
 <template>
   <div id="maincontent">
-    <div class="text">
-      <h1>Content goes here</h1>
+    <div id="banner">
+      <img src="../../public/img/jumbotron.jpg" alt="banner" />
     </div>
-    <div class="mainbar">
-        <MainBar />
+    <div id="title">
+      <p>CURRENT SERIES</p>
+    </div>
+    <div id="cards">
+      <CardComp v-for="(elm, i) in cards" :key="i" :card="elm" />
+    </div>
+    <div>
+      <button>LOAD MORE</button>
+    </div>
+    <div>
+      <MainBar />
     </div>
   </div>
 </template>
 
 <script>
+import cards from "../../public/jsons/dc-comics.json";
+import CardComp from "./CardComp.vue";
 import MainBar from "./MainBar.vue";
 
 export default {
   name: "MainContent",
-  components:{
-      MainBar
-  }
+  components: {
+    CardComp,
+    MainBar,
+  },
+  data() {
+    return {
+      cards: cards,
+    };
+  },
 };
 </script>
 
 <style lang="scss">
-#maincontent{
-    color: white;
-    background: #1c1c1c;
-    h1{
-        padding: 50px 15%;
+#maincontent {
+  color: white;
+  background: #1c1c1c;
+  #banner {
+    max-height: 400px;
+    overflow: hidden;
+    img {
+      width: 100%;
+    }
+  }
+  #title {
+    margin: -18px 15% 30px;
+    p {
+      font-weight: bold;
+      background: #0a82f9;
+      font-size: 24px;
+      display: inline;
+      padding: 10px 25px;
+    }
+  }
+  #cards{
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 15%;
+    }
+    button{
+      display: flex;
+      color: white;
+      font-weight: 800;
+      background: #0a82f9;
+      padding: 10px 75px;
+      margin: -30px auto 20px;
+      border: none;
+      cursor: pointer;
+      &:hover{
+        background: #2592ff;
+      }
     }
 }
-
 </style>
